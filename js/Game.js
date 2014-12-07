31f02fbd87303d4a2adbdd58c9c1a17b171f91ed
Joerg Hermann Mueller <nexyon@gmail.com>
2014-12-07 17:05:28 +1300

    Player movement, yay.

--- js/Game.js
+++ js/Game.js
@@ -19,14 +19,15 @@ VeggieWar.Game.prototype = {
         this.game.camera.update();
 
         this.player = this.game.add.sprite(1024, 600, 'player');
-        //this.player.anchor.setTo(0.5);
         this.game.physics.arcade.enable(this.player);
-        this.player.body.gravity.y = 200;
+        this.player.body.gravity.y = 400;
 
         this.cursors = this.game.input.keyboard.createCursorKeys();
-        //this.player.body.collideWorldBounds = true;
 
         this.game.physics.arcade.TILE_BIAS = 32;
+
+        this.move = {x: 0};
+        this.movetween = null;
     },
 
     update: function() {
@@ -49,20 +50,57 @@ VeggieWar.Game.prototype = {
 
         this.game.physics.arcade.collide(this.player, this.platformLayer);
 
+        var stoptween = true;
+
         if(this.player.body.onFloor()) {
             if(this.cursors.left.isDown) {
                 this.player.body.velocity.x = -100;
+                stoptween = false;
             }
             else if(this.cursors.right.isDown) {
                 this.player.body.velocity.x = 100;
+                stoptween = false;
             }
             else {
                 this.player.body.velocity.x = 0;
             }
 
             if(this.cursors.up.isDown) {
-                this.player.body.velocity.y = -200;
+                stoptween = true;
+                this.player.body.velocity.y = -300;
+                this.player.body.velocity.x *= 1.4;
             }
         }
+        else {
+            if(this.cursors.left.isDown) {
+                this.player.body.velocity.x -= this.game.time.physicsElapsed * 100;
+            }
+            else if(this.cursors.right.isDown) {
+                this.player.body.velocity.x += this.game.time.physicsElapsed * 100;
+            }
+        }
+
+        if(stoptween) {
+            if(this.movetween != null) {
+                if(this.movetween.isRunning) {
+                    this.movetween.repeatAll(0);
+                }
+                else {
+                    this.movetween = null;
+                }
+            }
+        }
+        else {
+            if(this.movetween == null) {
+                this.movetween = this.game.add.tween(this.move).to({x: 5}, 100, Phaser.Easing.Quadratic.Out).to({x: 0}, 100, Phaser.Easing.Quadratic.In).repeatAll(-1).start();
+            }
+            else {
+                this.movetween.repeatAll(-1);
+            }
+        }
+
+        this.player.position.y += this.player.body.offset.y;
+        this.player.body.offset.y = this.move.x;
+        this.player.position.y -= this.move.x;
     }
 }
